75b9a219722d914d1fc53ec35541607baa373d71
Izhan <babuizhan1234@gmail.com>
2021-10-13 15:37:51 +0530

    fix bugs 2

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post("/api/phoncase", async (req, res) => {
   res.json(response);
 });
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
